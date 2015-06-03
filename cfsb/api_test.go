2e456d52911ba9b349e8d3b6a02023ef21c2600f
高Gāo秀娇XiùJiāo <gaoxiujiao@gmail.com>
2015-06-03 09:39:20 -0400

    improving authortication test

--- cfsb/api_test.go
+++ cfsb/api_test.go
@@ -15,25 +15,30 @@ func TestAPIRequirements(t *testing.T) {
 	// basic_auth test, need username and password (Authentication :header) to do broker registrations
 	// return 401 Unauthorized if credentials are not valid  test, auth only tested here
 	// test when reject a request, response a 412 Precondition Failed message
-	httpHandlerFunc := http.HandlerFunc(httpAuth(EmptyHandler))
-	if r, err := http.NewRequest("GET", "", nil); err != nil {
-		t.Errorf("%v", err)
-	} else {
-		recorder := httptest.NewRecorder()
-		httpHandlerFunc.ServeHTTP(recorder, r)
-		if recorder.Code != http.StatusUnauthorized {
-			t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusUnauthorized)
-		}
+
+	var getBasicAuthTests = []struct {
+		username, password string
+		status             int
+	}{
+		{"cf", "cf", 200},
+		{"Aladdin", "open:sesame", 401},
+		{"", "", 401},
+		{"cf", "bala", 401},
+		{"", "cf", 401},
 	}
 
-	httpHandlerFunc = http.HandlerFunc(httpAuth(CatalogHandler))
-	if r, err := http.NewRequest("GET", "http://cf:cf@127.0.0.1:8080/v2/catalog", nil); err != nil {
-		t.Errorf("%v", err)
-	} else {
-		recorder := httptest.NewRecorder()
-		httpHandlerFunc.ServeHTTP(recorder, r)
-		if recorder.Code != http.StatusOK {
-			t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusOK)
+	httpHandlerFunc := http.HandlerFunc(httpAuth(CatalogHandler))
+	for _, autt := range getBasicAuthTests {
+		r, err := http.NewRequest("GET", "http://cf:cf@127.0.0.1:8080/v2/catalog", nil)
+		r.SetBasicAuth(autt.username, autt.password)
+		if err != nil {
+			t.Errorf("%v", err)
+		} else {
+			recorder := httptest.NewRecorder()
+			httpHandlerFunc.ServeHTTP(recorder, r)
+			if recorder.Code != autt.status {
+				t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusOK)
+			}
 		}
 	}
 
