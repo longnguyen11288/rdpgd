4b971ea42f8a654b577777109e134712bca34ef8
Wayne E. Seguin <wayneeseguin@gmail.com>
2015-06-03 10:20:15 -0400

    Code review with XJ

---
+++ rdpg/bdr.go
@@ -0,0 +1,121 @@
+package rdpg
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/wayneeseguin/rdpg-agent/log"
+)
+
+type RDPG struct {
+	URI string
+}
+
+// TODO: RDPG Struct => RDPG Struct, allowing for multiple instances of RDPG
+func NewRDPG(uri string) *RDPG {
+	if uri == "" || uri[0:13] != "postgresql://" {
+		log.Error(fmt.Sprintf("NewRDPG() uri malformed: '%s'", uri))
+		return nil
+	}
+	return &RDPG{URI: uri}
+}
+
+func (r *RDPG) Nodes() (nodes []Node) {
+	db, err := r.connect()
+	if err != nil {
+		log.Error(fmt.Sprintf("Nodes() %s", err))
+	}
+	rows, err := db.Query("SELECT host,port,user,'postgres' FROM rdpg.nodes;")
+	if err != nil {
+		log.Error(fmt.Sprintf("Nodes() %s", err))
+	} else {
+		sqlx.StructScan(rows, nodes)
+	}
+	return nodes
+}
+
+func (r *RDPG) CreateUser(username, password string) error {
+	for _, node := range r.Nodes() {
+		db, err := node.Connect()
+		if err != nil {
+			log.Error(fmt.Sprintf("CreateUser() %s", err))
+			return err
+		}
+
+		if _, err := db.Exec("CREATE USER ?;", username); err != nil {
+			log.Error(fmt.Sprintf("CreateUser() %s", err))
+		}
+
+		if _, err := db.Exec("ALTER USER ? ENCRYPTED PASSWORD '?';", username, password); err != nil {
+			log.Error(fmt.Sprintf("CreateUser() %s", err))
+		}
+		db.Close()
+	}
+	return nil
+}
+
+func (r *RDPG) CreateDatabase(name, owner string) error {
+	for _, node := range r.Nodes() {
+		db, err := node.Connect()
+		if err != nil {
+			log.Error(fmt.Sprintf("CreateDatabase() %s", err))
+			return err
+		}
+
+		if _, err := db.Exec(`CREATE DATABASE ? OWNER ? TEMPLATE='template0'`, name, owner); err != nil {
+			log.Error(fmt.Sprintf("CreateDatabase() %s", err))
+			return err
+		}
+
+		if _, err := db.Exec(`CREATE EXTENSION IF NOT EXISTS btree_gist`); err != nil {
+			log.Error(fmt.Sprintf("CreateDatabase() %s", err))
+			return err
+		}
+
+		if _, err := db.Exec(`CREATE EXTENSION IF NOT EXISTS bdr`); err != nil {
+			log.Error(fmt.Sprintf("CreateDatabase() %s", err))
+			return err
+		}
+		db.Close()
+	}
+	return nil
+}
+
+func (r *RDPG) CreateReplicationGroup(dbname string) error {
+	nodes := r.Nodes()
+	for index, node := range nodes {
+		db, err := node.Connect()
+		if err != nil {
+			log.Error(fmt.Sprintf("CreateReplicationGroup() %s", err))
+			return err
+		}
+		if index == 0 {
+			sq := `SELECT bdr.bdr_group_create(
+				local_node_name := '${vmName}${vmIndex}',
+				node_external_dsn := 'host=${nodes[0]} port=${pgbdrPort} user=${rdpgUser} dbname=${rdpgDB}'
+			); `
+			db.Exec(sq, node.Name, node.Host, node.Port, node.User, dbname)
+		} else {
+			sq := `SELECT bdr.bdr_group_join(
+				local_node_name := '${vmName}${vmIndex}',
+				node_external_dsn := 'host=${nodes[${vmIndex}]} port=${pgbdrPort} user=${rdpgUser} dbname=${rdpgDB}',
+				join_using_dsn := 'host=${nodes[0]} port=${pgbdrPort} user=${rdpgUser} dbname=${rdpgDB}'
+			); `
+			db.Exec(sq,
+				nodes[0].Name, nodes[0].Host, nodes[0].Port, nodes[0].User, dbname,
+				node.Host, node.Port, node.User, dbname)
+		}
+		db.Close()
+	}
+	// SELECT bdr.bdr_node_join_wait_for_ready();
+	return nil
+}
+
+func (r *RDPG) connect() (dr *sqlx.DB, err error) {
+	db, err := sqlx.Connect("postgres", r.URI)
+	if err != nil {
+		log.Error(fmt.Sprintf("rdpg.Node#Connect(): %s:\n%s\n", r.URI, err))
+		return db, err
+	}
+	return db, nil
+}
--- rdpg/schema.go
+++ rdpg/schema.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"database/sql"
+
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/wayneeseguin/rdpg-agent/log"
@@ -19,41 +20,46 @@ func init() {
 }
 
 func InitializeSchema() (err error) {
-	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() Connecting to DB URI: %s",rdpgURI))
+	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() Connecting to DB URI: %s", rdpgURI))
 
+	// For each node connect to pgbdr and:
+	//   CreatDatabase('rdpg','postgres')
+	//   "ALTER USER postgres SUPERUSER CREATEDB CREATEROLE INHERIT"
+	//   CreateReplicationGroup('rdpg')
 	db, err := sqlx.Connect("postgres", rdpgURI)
 	if err != nil {
 		log.Error(fmt.Sprintf("rdpg.InitializeSchema#Connect() %s:: %s\n", rdpgURI, err))
 		return err
 	}
 	defer db.Close()
-	
-	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() %s",SQL["rdpg_extensions"]))
+
+	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() %s", SQL["rdpg_extensions"]))
 	if _, err = db.Exec(SQL["rdpg_extensions"]); err != nil {
 		log.Error(fmt.Sprintf("rdpg.InitializeSchema(rdpg_extensions) %s\n", err))
 		return err
 	}
 
 	sq := "CREATE SCHEMA IF NOT EXISTS rdpg;"
-	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() %s",sq))
+	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() %s", sq))
 	if _, err = db.Exec(sq); err != nil {
 		log.Error(fmt.Sprintf("rdpg.InitializeSchema(create_table_services) %s\n", err))
 	}
 
 	// TODO: Check if table exists first and only run if it doesn't.
-	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() %s",SQL["create_table_rdpg_services"]))
+	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() %s", SQL["create_table_rdpg_services"]))
 	if _, err = db.Exec(SQL["create_table_rdpg_services"]); err != nil {
 		log.Error(fmt.Sprintf("rdpg.InitializeSchema(create_table_rdpg_services) %s\n", err))
 	}
 
 	// TODO: Check if table exists first and only run if it doesn't.
-	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() %s",SQL["create_table_rdpg_plans"]))
+	log.Debug(fmt.Sprintf("rdpg.InitializeSchema() %s", SQL["create_table_rdpg_plans"]))
 	if _, err = db.Exec(SQL["create_table_rdpg_plans"]); err != nil {
 		log.Error(fmt.Sprintf("rdpg.InitializeSchema(create_table_plans) %s\n", err))
 	}
 
 	var name string
 
+	// TODO: Move initial population of services out of rdpg-agent to Admin API.
 	if err := db.QueryRow("SELECT name FROM rdpg.services WHERE name='rdpg' LIMIT 1;").Scan(&name); err != nil {
 		if err == sql.ErrNoRows {
 			if _, err = db.Exec(SQL["insert_default_rdpg_services"]); err != nil {
@@ -66,6 +72,7 @@ func InitializeSchema() (err error) {
 		}
 	}
 
+	// TODO: Move initial population of services out of rdpg-agent to Admin API.
 	if err = db.QueryRow("SELECT name FROM rdpg.plans WHERE name='small' LIMIT 1;").Scan(&name); err != nil {
 		if err == sql.ErrNoRows {
 			if _, err = db.Exec(SQL["insert_default_rdpg_plans"]); err != nil {
