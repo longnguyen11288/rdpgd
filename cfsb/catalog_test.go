c1a9fb2f764543f91c5113759f8133b5ca3987dc
高Gāo秀娇XiùJiāo <gaoxiujiao@gmail.com>
2015-06-04 13:49:19 -0400

    adding catalogtest

---
+++ cfsb/catalog_test.go
@@ -0,0 +1,14 @@
+package cfsb
+
+import (
+	"testing"
+)
+
+func TestCatalog(t *testing.T) {
+	//c := Catalog{}
+	// c.Fetch()
+	//
+	// Next, setup a test such that it calls
+	// c.Fetch() for a Catalog c and then
+	// make sure that it has values that make sense.
+}
